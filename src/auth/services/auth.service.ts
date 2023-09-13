0940dd6833e33f56b349e0a43a0840c79277179c
Hanss Espinoza <hanssabinadie@gmail.com>
2023-09-12 22:04:31 -0600

    feat: add login enpoint

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 
-import { RegisterDto } from '../dto';
+import { LoginDto, RegisterDto } from '../dto';
 import { User } from 'src/users';
+import { MyResponse } from 'src/core';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +18,7 @@ export class AuthService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async register(registerDto: RegisterDto) {
+  async register(registerDto: RegisterDto): Promise<MyResponse<User>> {
     const { password, ...userData } = registerDto;
 
     const userVerificacion = await this.userRepository.findOne({
@@ -30,13 +35,57 @@ export class AuthService {
       });
 
       await this.userRepository.save(user);
-      delete user.password;
+      delete user.password, delete user.is_active;
 
-      return user;
+      const response: MyResponse<User> = {
+        statusCode: 201,
+        status: 'Created',
+        message: `Usuario con el email ${userData.email} ha sido creado con éxito`,
+        reply: user,
+      };
+
+      return response;
     } catch (error) {
       console.log(error);
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        password: true,
+        first_name: true,
+        last_name: true,
+        is_active: true,
+        email: true,
+      },
+    });
+
+    if (!user)
+      throw new UnauthorizedException('Las credenciales no son validas');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Las credenciales no son validas');
+
+    if (!user.is_active)
+      throw new UnauthorizedException(
+        'Usuario no activo, hable con el administrador',
+      );
+
+    delete user.password, delete user.is_active;
+
+    const response: MyResponse<User> = {
+      statusCode: 201,
+      status: 'Created',
+      message: 'Usuario encontrado con éxito',
+      reply: user,
+    };
+
+    return response;
   }
 
   private handleDBErrors(error: any): never {
