1546ef5ed8a243d490803956b5ee7935f6a2daa4
Hanss Espinoza <hanssabinadie@gmail.com>
2023-09-22 22:00:00 -0600

    feat: add checktoken

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -135,6 +135,15 @@ export class AuthService {
     }
   }
 
+  checkToken(user: User) {
+    const token = this.getJwtToken({ sub: user.user_id, user: user.email });
+
+    return {
+      user,
+      token,
+    };
+  }
+
   private getJwtToken(payload: JwtPayload): string {
     const token = this.jwtService.sign(payload);
     return token;
